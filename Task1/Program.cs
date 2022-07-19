22801ddcd13d20eee72d5b5dd05b57969eeac241
Roman <romatulupov95@mail.ru>
2022-07-19 20:51:31 +0300

    Square root

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter first number to compare");
+int numA = Convert.ToInt32(Console.ReadLine());
+int squareRoot = numA * numA;
+Console.WriteLine("Enter Square Root to compare");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numB == squareRoot)
+{
+    Console.WriteLine("NumB is a square root of NumA");
+}
+else
+{
+    Console.WriteLine("NumB is not a square root of NumA");
+}
